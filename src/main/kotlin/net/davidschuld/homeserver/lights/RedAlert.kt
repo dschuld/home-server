6ff5d0601b335874e19bfb945036605fa249d371
dschuld-mhp <net.david.schuld@mhp.com>
2023-07-24 20:37:23 +0200

    Implemented red alert audio

--- src/main/kotlin/net/davidschuld/homeserver/lights/RedAlert.kt
+++ src/main/kotlin/net/davidschuld/homeserver/lights/RedAlert.kt
@@ -10,45 +10,72 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.File
 
 class RedAlert : CoroutineScope by CoroutineScope(Dispatchers.IO) {
     private val httpClient = HttpClient { }
     private val serviceUrl = System.getenv("BRIDGE_URL")
 
     fun start() {
-        println("Red Alert not implemented yet")
         // launch an external tool via command line
 
         // send API call to hue bridge to start red lights
         println("Sending LIGHTS OFF to living room")
         launch {
-            httpClient.put<Unit>("$serviceUrl/groups/10/action") {
+            httpClient.put<Unit>("$serviceUrl/groups/1/action") {
                 contentType(ContentType.Application.Json)
                 body = """{ "on": false }"""
             }
-            flashOnce()
-            flashOnce()
-            flashOnce()
+            val audio = async { playMp3("tng_red_alert1.mp3") }
+            val lights = async { flashOnce() }
+            audio.await() to lights.await()
+            httpClient.put<Unit>("$serviceUrl/groups/1/action") {
+                contentType(ContentType.Application.Json)
+                body = """{ "on": false }"""
+            }
+            println("Done")
+//            flashOnce()
+//            flashOnce()
+        }
+    }
+
+    private fun playMp3(fileName: String) {
+        println("Playing $fileName")
+        val classLoader = Thread.currentThread().contextClassLoader
+        val resource = classLoader.getResource(fileName)
+        if (resource != null) {
+            val processBuilder = ProcessBuilder("mpg123", resource.path)
+
+            // you may also need to adjust the working directory if mpg123 doesn't recognize the path
+            processBuilder.directory(File(System.getProperty("user.dir")))
+
+            try {
+                val process = processBuilder.start()
+                val exitCode = process.waitFor()
+                println("Exited with code $exitCode")
+            } catch (e: Exception) {
+                println("An error occurred while trying to play the file $fileName")
+                e.printStackTrace()
+            }
+        } else {
+            println("The file $fileName does not exist")
         }
     }
 
     private suspend fun flashOnce() {
-        coroutineScope {
-            launch {
-                httpClient.put<Unit>("$serviceUrl/lights/8/state") {
-                    contentType(ContentType.Application.Json)
-                    body = requestBody(true)
-                }
-            }
-            delay(1000)
-            launch {
-                httpClient.put<Unit>("$serviceUrl/lights/8/state") {
-                    contentType(ContentType.Application.Json)
-                    body = requestBody(false)
-                }
-            }
-            delay(1000)
+        println("Flashing lights")
+        //TODO why does this not work when it works in Postman with Alert Lightstrip
+        httpClient.put<Unit>("$serviceUrl/lights/8/state") {
+            contentType(ContentType.Application.Json)
+            body = requestBody(true)
         }
+//                httpClient.put<Unit>("$serviceUrl/lights/8/state") {
+//                    contentType(ContentType.Application.Json)
+//                    body = requestBody(false)
+//                }
+//            }
+//            delay(1000)
+//        }
     }
 
     fun stop() {
@@ -65,6 +92,7 @@ fun requestBody(status: Boolean) =
         "on": $status,
         "hue": 0,
         "sat": 254,
-        "bri": 254
-    };
+        "bri": 254,
+        "alert": "lselect"
+    }
     """.trimIndent()
